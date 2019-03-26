5f3f9040ae641744fe6dbfb05e00d099da14f3d6
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-03-26 20:29:44 +0100

    Add fetch

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = (props) => {
+   return (
+       <button onClick={props.click}>Dodaj 5 uzytkowników</button>
+   ) 
+}
+
+export default Button;
---
+++ src/components/UsersList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const UsersList = (props) => {
+    
+    const users = props.users.map(user => (
+        <div key={user.login.uuid}>
+            <h4>{user.name.first} {user.name.last}</h4>
+            <p>{user.email}</p>
+        </div>
+    ))
+  return (
+      <ul>
+       {users}
+    </ul>
+    )
+}
+
+export default UsersList;
