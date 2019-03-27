d5ed8f672aa0ad2a63fa79782cd12de879118058
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-03-27 12:15:15 +0100

    Updates

---
+++ precache-manifest.3c135888730349674d03cc5d2876be31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a7b4707d15b05706eb",
+    "url": "/react-random-user/static/js/runtime~main.2e8e1514.js"
+  },
+  {
+    "revision": "9fd372dd9d69b367aa65",
+    "url": "/react-random-user/static/js/main.8265bcc9.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/react-random-user/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "9fd372dd9d69b367aa65",
+    "url": "/react-random-user/static/css/main.83aa881d.chunk.css"
+  },
+  {
+    "revision": "66bc8956b70e0131bd37b0455b55fedb",
+    "url": "/react-random-user/index.html"
+  }
+];
